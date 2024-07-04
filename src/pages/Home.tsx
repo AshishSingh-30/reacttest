5c10c3139dbaf3d47499dd49cf74b19ac484d492
Ashish Singh <ashish.ksingh8765@gmail.com>
2024-07-04 11:05:32 +0530

    testing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,13 @@
-
 import Dashboard from "@/components/Dashboard";
 
 const Home = () => {
   return (
     <div className="home">
       <Dashboard />
+      <p>
+        hii Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime illo
+        earum quos laborum dolores exercitationem vero possimus et in quaerat.
+      </p>
     </div>
   );
 };
